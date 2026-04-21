baa362b9fc651ebf0ca82d313e9effd35f599966
zuzannakalawska <zuziavoss@gmail.com>
2026-04-21 16:36:15 +0200

    Added a test function with fake environmental data

--- app/src/modules/cloud/cloud.c
+++ app/src/modules/cloud/cloud.c
@@ -538,7 +538,7 @@ static int request_storage_batch_data(uint32_t session_id)
 static int drain_environmental_stream_raw(void)
 {
     int err;
-    uint64_t timestamp_ms = 1677836800000;
+    uint64_t timestamp_ms = 1776766857812;
 
     struct test_data {
         uint64_t timestamp;
